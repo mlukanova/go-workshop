817e1516d5b08d08dc72f4e41211eb000bf47d2f
Meglena Lukanova <mlukanova@axway.com>
2018-10-06 11:30:22 +0300

    Initial router

--- cmd/go-workshop/main.go
+++ cmd/go-workshop/main.go
@@ -3,14 +3,23 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	log.Print("Hello, World!")
 
-	err := http.ListenAndServe(":8888", nil)
+	router := mux.NewRouter()
+	router.HandleFunc("/", Hello)
+
+	err := http.ListenAndServe(":8888", router)
 
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
 }
